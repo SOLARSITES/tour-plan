d5eae700977760f197a6b0b135bb46d40ac90181
SOLARSITES <iourion@gmail.com>
2020-11-23 19:25:41 +0300

    create: added 11 additional photos in Swiper slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
   loop: true,
 
-  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    prevEl: ".slider-button--prev",
+    nextEl: ".slider-button--next",
   },
+
+  speed: 800,
 });
