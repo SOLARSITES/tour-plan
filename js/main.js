aaeb06f10171311f099ef34cca07831b1166101a
SOLARSITES <iourion@gmail.com>
2020-12-02 00:43:37 +0300

    create: added mobile menu for screens 768px-576px-320px

--- js/main.js
+++ js/main.js
@@ -39,3 +39,8 @@ $(window).scroll(function () {
     max: 100,
   });
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
