edc16843b3cfe316071f4a0be22d21b3531422cf
SOLARSITES <iourion@gmail.com>
2020-12-04 18:39:09 +0300

    create: added modal dialog for block .rating in .section.hotel

--- js/main.js
+++ js/main.js
@@ -1,46 +1,67 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  loop: true,
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    loop: true,
 
-  navigation: {
-    prevEl: ".hotel-slider__button--prev",
-    nextEl: ".hotel-slider__button--next",
-  },
+    navigation: {
+      prevEl: ".hotel-slider__button--prev",
+      nextEl: ".hotel-slider__button--next",
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
 
-  speed: 750,
-});
-
-var reviewsSlider = new Swiper(".reviews-slider", {
-  loop: true,
-
-  navigation: {
-    prevEl: ".reviews-slider__button--prev",
-    nextEl: ".reviews-slider__button--next",
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-
-  speed: 750,
-});
-
-$(window).scroll(function () {
-  $(".newsletter").bgscroll({
-    direction: "top",
-    bgpositionx: 50,
-    debug: false,
-    min: 0,
-    max: 100,
+    speed: 750,
   });
-});
 
-const menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", () =>
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
-);
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    loop: true,
+
+    navigation: {
+      prevEl: ".reviews-slider__button--prev",
+      nextEl: ".reviews-slider__button--next",
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    speed: 750,
+  });
+
+  const menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", () =>
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
+  );
+
+  $(window).scroll(function () {
+    $(".newsletter").bgscroll({
+      direction: "top",
+      bgpositionx: 50,
+      debug: false,
+      min: 0,
+      max: 100,
+    });
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
